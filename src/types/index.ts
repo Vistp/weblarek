565bd542b5a1a411020a57fe3b3330372d15680a
Irina Varushkina <rnvr@mail.ru>
2026-07-22 22:01:17 +0500

    Добавляет интерфесы карточки товара и покупателя, тип способа оплаты

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,24 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+/** Карточка товара */
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+/** Покупатель */
+export interface IBuyer {
+  payment: TPayment;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+/** Способ оплаты */
+export type TPayment = 'card' | 'cash' | '';
