5b353cd410b0b7135e05e3621603ed80fb1f6fe9
Irina Varushkina <rnvr@mail.ru>
2026-08-07 21:23:24 +0500

    ПР-9 Обрабатывает изменение выбранного для просмотра товара

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { Cart } from './components/models/Cart';
 import { Catalog } from './components/models/Catalog';
 import { ServerConnector } from './components/ServerConnector';
 import { CardCatalog } from './components/view/CardCatalog';
+import { CardPreview } from './components/view/CardPreview';
 import { Header } from './components/view/Header';
+import { Modal } from './components/view/Modal';
 import './scss/styles.scss';
+import { IProduct } from './types';
 import { API_URL } from './utils/constants';
 
 const events = new EventEmitter();
@@ -19,9 +22,12 @@ const buyer = new Buyer(events);
 
 const galleryContainer = document.querySelector('.gallery') as HTMLElement;
 const headerContainer = document.querySelector('.header') as HTMLElement;
+const modalContainer = document.querySelector('#modal-container') as HTMLElement;
 
 const header = new Header(headerContainer, events);
+const modal = new Modal(modalContainer, events);
 
+/** Изменение каталога товаров */
 events.on('items:changed', () => {
   const products = catalog.getProducts();
 
@@ -45,6 +51,30 @@ events.on('items:changed', () => {
   });
 
   galleryContainer.replaceChildren(...cardsList);
+});
+
+/** Изменение выбранного для просмотра товара */
+events.on('preview:changed', (product: IProduct) => {
+  const cardPreviewTemplate = document.querySelector('#card-preview') as HTMLTemplateElement;
+  const cardPreviewFragment = cardPreviewTemplate.content.cloneNode(true) as DocumentFragment;
+  const cardPreviewContainer = cardPreviewFragment.firstElementChild as HTMLElement;
+
+  const cardPreview = new CardPreview(cardPreviewContainer, {
+    onClick: () => {},
+    onAction: () => {
+      events.emit('card:add-to-cart', product);
+    }
+  });
+
+  cardPreview.title = product.title;
+  cardPreview.price = product.price;
+  cardPreview.category = product.category;
+  cardPreview.image = product.image;
+  cardPreview.text = product.description;
+  cardPreview.buttonText = 'В корзину';
+
+  modal.content = cardPreview.render();
+  modal.open();
 });
 
 serverConnector.getProducts()
